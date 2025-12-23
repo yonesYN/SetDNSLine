8190741e8a89164e5cff37d1ee247a9d707a63df
yonesYN <132742251+yonesYN@users.noreply.github.com>
2025-12-23 12:23:55 -0800

    v1.0

--- SetDNSLine.bat
+++ SetDNSLine.bat
@@ -1,31 +1,50 @@
-@echo off
-title "Set DNS Line"
-mode con: cols=83 lines=19
-setlocal enabledelayedexpansion
-whoami /groups | findstr "S-1-16-12288" >nul && GOTO S
-set "params=%*"
-cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+@ECHO OFF
+TITLE "Set DNS Line"
+MODE con: cols=83 lines=19
+SETLOCAL ENABLEDELAYEDEXPANSION
+WHOAMI /GROUPS | findstr "S-1-16-12288" >NUL && GOTO S
+SET "params=%*"
+CD /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>NUL 2>NUL || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
 :S
-cd /d "%~dp0"
-if not exist "config.txt" (
-	GOTO ERROR
-)
-CLS
-ECHO Loading...
-for /f "tokens=2 delims=:" %%a in ('findstr /b "INTERFACE" "config.txt"') do (
-	set "cfg=%%~a"
+CD /d "%~dp0"
+IF NOT EXIST "config.txt" (GOTO ERROR)
+FOR /F "tokens=2 delims=:" %%a in ('findstr /b "INTERFACE" "config.txt"') do (
+	set "i=%%a"
 	GOTO M
 )
+:SEL
+CLS
+SET "ct=0"
+FOR /F "tokens=3*" %%a in ('netsh interface show interface ^| findstr /c:"Connected"') do (
+	set /a ct+=1
+	set "if!ct!=%%b"
+	echo [!ct!] %%b
+)
+
+IF !ct!==0 (
+    echo No connected interfaces found
+	color 4
+    pause
+    exit
+)
+
+SET /p num="Interface: "
+IF NOT DEFINED if%num% (GOTO SEL)
+SET "i='!if%num%!'"
+
 :M
-if defined cfg (
-for /f "delims=" %%F in ('powershell -NoProfile -Command "$DNS=(Get-DnsClientServerAddress -InterfaceAlias %cfg% | Where AddressFamily -eq 2).ServerAddresses -join ', '; Write-Output (%cfg% + ': ' + $DNS + 'N')"') do (
+SET "i=%i:	=%"
+SET "m=%i: =%"
+IF NOT DEFINED i (GOTO SEL)
+IF NOT DEFINED m (GOTO SEL)
+IF /I "%i: =%"=="auto" (
+FOR /F "delims=" %%F in ('powershell -NoProfile -Command "$ap=(Get-NetAdapter | Where Status -eq 'Up' | Sort-Object InterfaceIndex)[0]; $DNS=(Get-DnsClientServerAddress -AddressFamily IPv4 -InterfaceIndex $ap.ifIndex).ServerAddresses -join ', '; Write-Output ($ap.Name + ': ' + $DNS + 'N')"') do (
     set "full=%%F"
-	GOTO I
+	GOTO I)
 )
-)
-for /f "delims=" %%F in ('powershell -NoProfile -Command "$ap=(Get-NetAdapter | Where Status -eq 'Up' | Sort-Object InterfaceIndex)[0]; $DNS=(Get-DnsClientServerAddress -InterfaceIndex $ap.ifIndex | Where AddressFamily -eq 2).ServerAddresses -join ', '; Write-Output ($ap.Name + ': ' + $DNS + 'N')"') do (
-    set "full=%%F"
+FOR /F "delims=" %%F in ('powershell -NoProfile -Command "$DNS=(Get-DnsClientServerAddress -AddressFamily IPv4 -InterfaceAlias %i% -ErrorAction SilentlyContinue).ServerAddresses -join ', '; Write-Output (%i% + ': ' + $DNS + 'N')"') do (
+	set "full=%%F"
 )
 :I
 SET "ap=%full:: =" & rem %
@@ -38,60 +57,56 @@ GOTO D
 SET "adns=%adns:N=%"
 SET "dns1=%adns:,=" & SET "dns2=%"
 
-set "name="
-set "pattern=:[ 	]*'%dns1%"
-if defined dns2 (
-	for /f "delims=" %%a in ('findstr /r /c:"%pattern%" "config.txt" ^| findstr "%dns2%"') do (
+SET "name="
+SET "pattern=:[ 	]*'%dns1%"
+IF DEFINED dns2 (
+	FOR /F "delims=" %%a in ('findstr /r /c:"%pattern%" "config.txt" ^| findstr "%dns2%"') do (
 		set "name=%%a"
 		GOTO N
 	)
 )
-for /f "delims=" %%a in ('findstr /r /c:"%pattern%" "config.txt" ^| findstr /v ","') do (
+FOR /F "delims=" %%a in ('findstr /r /c:"%pattern%" "config.txt" ^| findstr /v ","') do (
 	set "name=%%a"
 	GOTO N
 )
 :N
-if defined name (
+IF DEFINED name (
 SET "name=%name::=" & rem %
-)else (SET "name=DNS")
+)ELSE (SET "name=DNS")
 
-FOR /F "tokens=* USEBACKQ" %%F IN (`ping %dns1% -n 1 -w 1000`) DO (SET ping=%%F)
+FOR /F "tokens=9" %%F in ('ping -n 1 -w 1000 %dns1% ^| find "Minimum ="') do (SET ping=%%F)
 COLOR B
 CLS
-SET "ping=%ping:,="& rem %
-IF "%ping:~,1%" == "M" (ECHO %name%: %adns:N=%		%ping:Minimum =Ping%
-GOTO D)
-ECHO [96m%name%: %adns%		Ping=[31mTimeout[96m
-
+IF NOT DEFINED ping (SET "ping=[31mTimeout[96m")
+ECHO %name%: %adns:N=%		%ping%
+SET "ping="
 
 :D
-IF "%adns%"=="N" (ECHO [96mDNS: [31mNone[96m)
+IF "%adns%"=="N" (ECHO DNS: [31mNone[96m)
 ECHO Interface: %ap%
 ECHO:
-set "count="
-for /f "skip=1 tokens=1 delims=:" %%a in ('findstr /r "[0-9].[0-9]" "config.txt"') do (
-    set /a count+=1
-    echo [!count!] %%a
+SET "ct="
+FOR /F "skip=1 tokens=1 delims=:" %%a in ('findstr /r "[0-9].[0-9]" "config.txt"') do (
+    set /a ct+=1
+    echo [!ct!] %%a
 )
-set "dns="
+ECHO:
+ECHO [D] Defualt DNS   [S] Select Interface
+SET "dns="
 SET "lin=a"
 SET /p lin=type :
 
-echo %lin% | findstr /r "[0-9]" >nul
-if %errorlevel% equ 0 (
-	GOTO SET
-) else (
-	IF %lin%==d (GOTO DHCP)
-	IF %lin%==D (GOTO DHCP)
-	GOTO I
-)
+ECHO %lin% | findstr /r "[0-9]" >NUL && GOTO SET
+IF /I "%lin%"=="D" (GOTO DHCP)
+IF /I "%lin%"=="S" (GOTO SEL)
+GOTO I
 
 :SET
-for /f "skip=%lin% tokens=2 delims=:" %%a in ('findstr /r "[0-9].[0-9]" "config.txt"') do (
-	set "dns=%%~a"
+FOR /F "skip=%lin% tokens=2 delims=:" %%a in ('findstr /r "[0-9].[0-9]" "config.txt"') do (
+	set "dns=%%a"
 	GOTO NE
 )
-if not defined dns ( GOTO I )
+if not defined dns (GOTO I)
 :NE
 SET "dns=%dns: =%"
 SET "dns=%dns:	=%"
@@ -107,18 +122,18 @@ ECHO     888     888 Y88b888
 ECHO     888     888  Y88888
 ECHO     888     888   Y8888
 ECHO     888     888    Y888
-powershell -NoProfile -Command "Set-DnsClientServerAddress -InterfaceAlias '%ap%' -ServerAddresses (%dns%)" >nul && COLOR A || COLOR 4
-ipconfig /flushdns >nul
+powershell -NoProfile -Command "Set-DnsClientServerAddress -InterfaceAlias '%ap%' -ServerAddresses (%dns%)" >NUL && COLOR A || COLOR 4
+ipconfig /flushdns >NUL
 GOTO M
 
 :DHCP
 CLS
 ECHO Loading...
-netsh interface ipv4 set dnsservers name="%ap%" source=dhcp >nul && COLOR A || COLOR 4
-ipconfig /flushdns >nul
+netsh interface ipv4 set dnsservers name="%ap%" source=dhcp >NUL && COLOR A || COLOR 4
+ipconfig /flushdns >NUL
 GOTO M
 
 :ERROR
 ECHO [31mconfig.txt not exist
-timeout 6 >nul
-exit
+timeout 6 >NUL
+EXIT
